f971d8a5a10ee6d2f8e3bb3a7c4df8db2dc10b7d
IndimaDias <indimad@yahoo.com>
2019-05-02 20:31:10 -0400

    version with button array and images populated when button clicked

--- assets/javascript/searchJs.js
+++ assets/javascript/searchJs.js
@@ -1,10 +1,32 @@
 $(function(){
-    $("#btnSubmit").on("click", function(event){
+
+    var charcterArray = ["Tinkerbel","Cinderella", "Shophia","Shophia","Shophia","Shophia","Shophia","Shophia","Shophia",
+    "Shophia","Shophia","Shophia","Shophia","Shophia","Shophia","Shophia","Shophia",];
+    var btnDiv  = $("#btnSection");
+    btnLoad();
+
+    function btnLoad(){
+        //This function will read the array and load the buttons to the jumbotran
+
+        for (var i=0; i<charcterArray.length; i++){
+            var newButton = $("<button>");            
+            newButton.text(charcterArray[i]);
+            newButton.attr("data-name",charcterArray[i]);
+            newButton.addClass("btn text-danger border-bottom font-weight-bolder p-0 m-2 btnQueryVal");
+            console.log(newButton);
+            btnDiv.append(newButton);
+        }
+    };
+
+
+    $(".btnQueryVal").on("click", function(event){
         event.preventDefault();
+        debugger;
         var apiKey = "HH2R2kthUYB18vT3SRR2ePHdC3z3SJpB&q";
 
         var queryURL = ""; 
-        var queryValue = $("#searchVal").val();
+        var queryValue = $(this).attr("data-name");
+        console.log(queryValue);
         var results;
         if (queryValue != null) {
             queryURL = "https://api.giphy.com/v1/gifs/search?api_key=" + apiKey + 
@@ -16,7 +38,7 @@ $(function(){
             url : queryURL,
             method : "GET"
         }).then (function(response){
-            debugger;
+         
             results = response.data;
             var divRow = $("<div>");
             divRow.addClass("row mt-1 mb-1");
